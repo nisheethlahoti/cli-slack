84770c8fbaea6aa0d8cb1b236997e7f7f80965b5
Nisheeth Lahoti <nisheeth.lahoti@gmail.com>
2018-01-31 08:21:02 +0530

    Initial basic tool to use slack from python cli. Supports basic message-posting, file-uploading, incoming events. Keeps connection alive when idle. Horrible UX, though.

---
+++ whatUdoingbot.py
@@ -0,0 +1,56 @@
+import sys
+import time
+from slackclient import SlackClient
+
+slack_client = SlackClient(sys.argv[1])
+
+class User:
+    def __init__(self, data):
+        self.id = data['id']
+        self.name = data['name']
+
+    def message(self):
+        slack_client.api_call("chat.postMessage", channel=self.id, as_user=True,
+                              text="".join(sys.stdin.readlines())[:-1])
+        idle_loop()
+
+    def upload(self, fname):
+        with open(fname) as msgfile:
+            slack_client.api_call("files.upload", channels=self.id, file=msgfile)
+        idle_loop()
+
+
+ignore_types = {'hello', 'user_typing', 'desktop_notification', 'im_marked', 'update_thread_state'}
+def receive():
+    for messages in iter(slack_client.rtm_read, []):
+        for json in messages:
+            if json['type'] == 'message' and 'user' in json:
+                uname = users[json['user']].name
+                if uname != slack_client.server.username:
+                    print( (uname + ':\n' + json['text']).replace('\n', '\n| ') + '\n')
+            elif json['type'] not in ignore_types:
+                print(json)
+                print()
+
+def idle_loop():
+    print("[Idling about]")
+    while True:
+        try:
+            receive()
+            time.sleep(1)
+        except Exception as e:
+            print("Disconnected: {}".format(e))
+            while not slack_client.rtm_connect():
+                print("Unable to connect")
+                time.sleep(1)
+            print("Slack client connected")
+
+users = {}
+for user_data in slack_client.api_call("users.list")['members']:
+    if not user_data['deleted']:
+        user = User(user_data)
+        users[user.id] = user
+        globals()[user.name] = user
+
+slack_client.rtm_connect()
+idle_loop()
