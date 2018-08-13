65bc6fbf803f4c2f5753e3f0ee509b6c6ff71c43
Nisheeth Lahoti <nisheeth.lahoti@gmail.com>
2018-08-13 12:18:58 +0530

    Running receive in background thread, thereby improving UX drastically

--- cli-slack.py
+++ cli-slack.py
@@ -4,90 +4,81 @@ import requests
 import sys
 import time
 from slackclient import SlackClient
+from threading import Thread
 
-slack_client = SlackClient(sys.argv[1])
+slack = SlackClient(sys.argv[1])
 locations = {}
 files = []
+ignore_types = {
+    'hello', 'user_typing', 'desktop_notification', 'update_thread_state', 'im_marked',
+    'mpim_marked', 'group_marked', 'channel_marked', 'pref_change', 'file_created'
+}
 
 
 class PostLocation:
     def __init__(self, data, kind):
+        self.data = data
         self.id = data['id']
         self.name = data['name']
         self.kind = kind
 
     def message(self, msg):
-        slack_client.api_call("chat.postMessage", channel=self.id, as_user=True, text=msg)
-        idle_loop()
+        slack.api_call("chat.postMessage", channel=self.id, as_user=True, text=msg)
 
     def write_message(self):
         self.message("".join(sys.stdin.readlines())[:-1])
 
     def upload(self, fname):
         with open(fname) as msgfile:
-            slack_client.api_call("files.upload", channels=self.id, file=msgfile)
-        idle_loop()
-
-
-ignore_types = {
-    'hello', 'user_typing', 'desktop_notification', 'update_thread_state', 'im_marked',
-    'mpim_marked', 'group_marked', 'channel_marked'
-}
+            slack.api_call("files.upload", channels=self.id, file=msgfile)
 
 
 def receive():
     global users
-    for messages in iter(slack_client.rtm_read, []):
-        for json in messages:
-            if json['type'] == 'message' and 'user' in json:
-                uname = users[json['user']].name
-                if uname != slack_client.server.username:
-                    if json['channel'] in locations:
-                        uname += " in " + locations[json['channel']].name
-                    print((uname + ':\n' + json['text']).replace('\n', '\n| ') + '\n')
-            elif json['type'] == 'file_shared' and 'file_id' in json:
-                lfile = slack_client.api_call('files.info', file=json['file_id'])['file']
-                print(f"File #{len(files)} uploaded: {lfile['name']}\
-                       \n  Size: {lfile['size']}\n  User: {lfile['user']}")
-                files.append(lfile)
-            elif json['type'] not in ignore_types:
-                print(json)
-                print()
+    while True:
+        time.sleep(1)
+        for messages in iter(slack.rtm_read, []):
+            for json in messages:
+                if json['type'] == 'message' and 'user' in json:
+                    uname = users[json['user']].name
+                    if uname != slack.server.username:
+                        if json['channel'] in locations:
+                            uname += " in " + locations[json['channel']].name
+                        print('\n' + (uname + ':\n' + json['text']).replace('\n', '\n| '))
+                elif json['type'] == 'file_shared' and 'file_id' in json:
+                    lfile = slack.api_call('files.info', file=json['file_id'])['file']
+                    print(f"\nFile #{len(files)} uploaded: {lfile['name']}\
+                           \n  Size: {lfile['size']}\n  User: {lfile['user']}")
+                    files.append(lfile)
+                elif json['type'] not in ignore_types:
+                    print(f'\nUnknown notification: {json}')
 
 
 def download(fnum, name=''):
-    finfo = files[fnum]
-    headers = {'Authorization': 'Bearer ' + slack_client.token}
-    with requests.get(finfo['url_private_download'], headers=headers) as fle:
-        with open(name or finfo['name'], 'wb') as outf:
+    url = files[fnum]['url_private_download']
+    with requests.get(url, headers={'Authorization': 'Bearer ' + slack.token}) as fle:
+        with open(name or files[fnum]['name'], 'wb') as outf:
             outf.write(fle.content)
 
 
-def idle_loop():
-    print("[Idling about]")
-    while True:
-        try:
-            receive()
-            time.sleep(1)
-        except Exception:
-            print("Disconnected")
-            if not slack_client.rtm_connect():
-                break
-            print("Reconnected")
+def connect():
+    print('Establishing real-time messaging connection...')
+    slack.rtm_connect(auto_reconnect=True)
+    Thread(target=receive, daemon=True).start()
 
 
 def populate(kind, getter, invalidator):
     globals()[kind] = {}
-    for data in slack_client.api_call(kind + ".list")[getter]:
+    for data in slack.api_call(kind + ".list")[getter]:
         if not data[invalidator]:
             loc = PostLocation(data, kind)
             globals()[kind][loc.id] = loc
             globals()[loc.name.replace('-', '_')] = loc
     locations.update(globals()[kind])
+    print(f'Identified {kind} {", ".join(x.name for x in globals()[kind].values())}')
 
 
 populate('users', 'members', 'deleted')
 populate('groups', 'groups', 'is_archived')
 populate('channels', 'channels', 'is_archived')
-slack_client.rtm_connect()
-idle_loop()
+connect()
