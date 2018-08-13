53c90ce0080ec66232c4f2cfd8a080bdee60b868
Nisheeth Lahoti <nisheeth.lahoti@gmail.com>
2018-08-13 22:15:30 +0530

    Making handling mpdms easier.

--- cli-slack.py
+++ cli-slack.py
@@ -16,11 +16,10 @@ ignore_types = {
 
 
 class PostLocation:
-    def __init__(self, data, kind):
+    def __init__(self, data):
         self.data = data
         self.id = data['id']
         self.name = data['name']
-        self.kind = kind
 
     def message(self, msg):
         slack.api_call("chat.postMessage", channel=self.id, as_user=True, text=msg)
@@ -32,25 +31,26 @@ class PostLocation:
         with open(fname) as msgfile:
             slack.api_call("files.upload", channels=self.id, file=msgfile)
 
+    def __repr__(self):
+        return f'{self.__class__.__name__}(id={self.id}, name={self.name})'
+
 
 def receive():
-    global users
     while True:
         time.sleep(1)
         for messages in iter(slack.rtm_read, []):
             for json in messages:
-                if json['type'] == 'message' and 'user' in json:
+                if json['type'] == 'message' and 'user' in json and json['user'] != uid:
                     uname = users[json['user']].name
-                    if uname != slack.server.username:
-                        if json['channel'] in locations:
-                            uname += " in " + locations[json['channel']].name
-                        print('\n' + (uname + ':\n' + json['text']).replace('\n', '\n| '))
+                    if json['channel'] in locations:
+                        uname += " in " + locations[json['channel']].name
+                    print('\n' + (uname + ':\n' + json['text']).replace('\n', '\n| '))
                 elif json['type'] == 'file_shared' and 'file_id' in json:
                     lfile = slack.api_call('files.info', file=json['file_id'])['file']
                     print(f"\nFile #{len(files)} uploaded: {lfile['name']}\
-                           \n  Size: {lfile['size']}\n  User: {lfile['user']}")
+                           \n  Size: {lfile['size']}\n  User: {users[lfile['user']].name}")
                     files.append(lfile)
-                elif json['type'] not in ignore_types:
+                elif json['type'] not in ignore_types and json.get('user') != uid:
                     print(f'\nUnknown notification: {json}')
 
 
@@ -67,18 +67,27 @@ def connect():
     Thread(target=receive, daemon=True).start()
 
 
-def populate(kind, getter, invalidator):
-    globals()[kind] = {}
-    for data in slack.api_call(kind + ".list")[getter]:
-        if not data[invalidator]:
-            loc = PostLocation(data, kind)
-            globals()[kind][loc.id] = loc
-            globals()[loc.name.replace('-', '_')] = loc
-    locations.update(globals()[kind])
-    print(f'Identified {kind} {", ".join(x.name for x in globals()[kind].values())}')
+def populate(api, getter, skip, first_name, second_name='', splitter=lambda _: True):
+    elems = {x['id']: PostLocation(x) for x in slack.api_call(api)[getter] if not x[skip]}
+    first = {k: v for k, v in elems.items() if splitter(v)}
+    second = {k: v for k, v in elems.items() if k not in first}
+    globals().update({x.name.replace('-', '_'): x for x in elems.values()})
+    locations.update(elems)
+    for vals, name in (first, first_name), (second, second_name):
+        if vals:
+            print(f'Identified {name} {", ".join(x.name for x in vals.values())}')
+    return first, second
 
 
-populate('users', 'members', 'deleted')
-populate('groups', 'groups', 'is_archived')
-populate('channels', 'channels', 'is_archived')
+def mpdm(*args):
+    groups = [x for x in mpdms.values() if set(x.data['members']) == {y.id for y in args} | {uid}]
+    assert len(groups) == 1, f'No unique group with these users {[x.name for x in groups]}'
+    return groups[0]
+
+
+users, _ = populate('users.list', 'members', 'deleted', 'users')
+public_channels, _ = populate('channels.list', 'channels', 'is_archived', 'public channels')
+private_channels, mpdms = populate('groups.list', 'groups', 'is_archived', 'private channels',
+                                   'mpdms', lambda x: x.name[:5] != 'mpdm-')
 connect()
+uid = slack.server.login_data['self']['id']
